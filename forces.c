fb4ff772895c968f08ae4ee1b0e2400f059dac5e
pcs13_19 <lighteningman@gmail.com>
2014-01-25 20:50:16 +0100

    Works but slow

--- forces.c
+++ forces.c
@@ -17,13 +17,16 @@
     #pragma omp for private(i, j) reduction(+:vir, epot) schedule(dynamic, 10)//schedule(guided)
     for (i=0; i<npart*3; i+=3) {
 
-      // zero force components on particle i
-      // double f_local[npart * 3];
-      // memset(f_local, 0, npart * 3);
-
       double fxi = 0.0;
       double fyi = 0.0;
       double fzi = 0.0;
+
+      double f_private[npart*3-(i+3)];
+      // memset(f_private, 0.0f, 3);
+      int ape;
+      for(ape=0; ape < npart*3-(i+3); ape++)
+        f_private[ape] = 0;
+
 
       // loop over all particles with index > i
 
@@ -41,8 +44,8 @@
 
         double rd = xx*xx+yy*yy+zz*zz;
 
-  // if distance is inside cutoff radius compute forces
-  // and contributions to pot. energy and virial
+        // if distance is inside cutoff radius compute forces
+        // and contributions to pot. energy and virial
 
         if (rd<=rcoff*rcoff) {
 
@@ -52,49 +55,62 @@
           double r148     = rrd4*(rrd3 - 0.5);
 
 
-          // epot_local    += rrd3*(rrd3-1.0);
-          // vir_local     -= rd*r148;
-          // #pragma omp critical
-          // {
-            epot    += rrd3*(rrd3-1.0);
-            vir     += -rd*r148;
-          // }
+          epot    += rrd3*(rrd3-1.0);
+          vir     += -rd*r148;
 
           fxi     += xx*r148;
           fyi     += yy*r148;
           fzi     += zz*r148;
 
 
-          // f_local[j]    -= xx*r148;
-          // f_local[j+1]  -= yy*r148;
-          // f_local[j+2]  -= zz*r148;
-          #pragma omp atomic
-          f[j]    -= xx*r148;
-          #pragma omp atomic
-          f[j+1]  -= yy*r148;
-          #pragma omp atomic
-          f[j+2]  -= zz*r148;
+          // #pragma omp atomic
+          // f[j]    -= xx*r148;
+          // #pragma omp atomic
+          // f[j+1]  -= yy*r148;
+          // #pragma omp atomic
+          // f[j+2]  -= zz*r148;
+
+//                     if (i == 0) {
+//       printf("local: %f, global: %f \n", f_private[0], f_private[1]);
+// }
+
+
+          f_private[j-(i+3)] += xx*r148;
+          f_private[j+1-(i+3)] += yy*r148;
+          f_private[j+2-(i+3)] += zz*r148;
+
+
+
+
+
 
         }
 
       }
 
+      for(ape=0; ape < npart*3-(i+3); ape++)
+        #pragma omp atomic
+        f[ape+(i+3)] -= f_private[ape];
+          // #pragma omp atomic
+          // f[j] -= f_private[j];
+          // #pragma omp atomic
+          // f[j+1] -= f_private[j+1];
+          // #pragma omp atomic
+          // f[j+2] -= f_private[j+2];
+
       // update forces on particle i
 
-      // f_local[i]     += fxi;
-      // f_local[i+1]   += fyi;
-      // f_local[i+2]   += fzi;
       #pragma omp atomic
       f[i]     += fxi;
       #pragma omp atomic
       f[i+1]   += fyi;
       #pragma omp atomic
       f[i+2]   += fzi;
-
-
-      // for(i=0; i< npart * 3; i++)
-      //   #pragma omp atomic
-      //   f[i] += f_local[i];
+//           if (i == 0) {
+//       printf("local: %f, global: %f \n", f_local[i], f[i]);
+//       printf("local: %f, global: %f \n", f_local[i+1], f[i+1]);
+//       printf("local: %f, global: %f \n", f_local[i+2], f[i+2]);
+//}
 
     } // end of for loop
   }
